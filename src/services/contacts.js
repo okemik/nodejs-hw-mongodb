80ebe501c80fd52ee5e6097dcca32463dfafeaf4
okemik <okemik@yahoo.com>
2025-07-07 01:37:45 +0200

    9.th

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,6 +2,8 @@ const Contact = require("../models/Contact");
 
 const createContact = (data) => Contact.create(data);
 
+const getAll = () => Contact.find({}); // Buraya ekle
+
 const getContactById = (id) => Contact.findById(id);
 
 const updateContact = (id, data) =>
@@ -11,6 +13,7 @@ const deleteContact = (id) => Contact.findByIdAndDelete(id);
 
 module.exports = {
   createContact,
+  getAll,           // Burayı da ekle
   getContactById,
   updateContact,
   deleteContact,
