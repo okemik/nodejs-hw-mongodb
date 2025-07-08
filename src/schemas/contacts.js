d97bba1f8d6b4efa658f414dae59ce9f662406e5
okemik <okemik@yahoo.com>
2025-07-09 01:45:20 +0200

    schemas/contacts.js updated

--- src/schemas/contacts.js
+++ src/schemas/contacts.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const addSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid("family", "friend", "work", "other")
 });
@@ -11,7 +11,7 @@ const addSchema = Joi.object({
 const updateSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   email: Joi.string().email(),
-  phone: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid("family", "friend", "work", "other")
 }).min(1);
