a86b858e643c1edb875c29fbd4c96b333bdd5a60
okemik <okemik@yahoo.com>
2025-07-10 00:43:44 +0200

    server.js updated

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const app = require('./src/app');
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGODB_URI)
