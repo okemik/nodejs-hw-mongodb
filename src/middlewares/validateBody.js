0fa717fecb26710a4652c71faa2e8137da618a2c
okemik <okemik@yahoo.com>
2025-07-09 02:40:00 +0200

    Add validateBody middleware

---
+++ src/middlewares/validateBody.js
@@ -0,0 +1,14 @@
+// src/middlewares/validateBody.js
+const createHttpError = require("http-errors");
+
+const validateBody = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return next(createHttpError(400, error.message));
+    }
+    next();
+  };
+};
+
+module.exports = validateBody;
