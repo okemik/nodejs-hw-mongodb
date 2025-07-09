578685233f2240a53cf3bc2616f6083faa83aeba
okemik <okemik@yahoo.com>
2025-07-09 02:37:49 +0200

    Add ctrlWrapper utility

---
+++ src/utils/ctrlWrapper.js
@@ -0,0 +1,13 @@
+// src/utils/ctrlWrapper.js
+const ctrlWrapper = (ctrl) => {
+    return async (req, res, next) => {
+      try {
+        await ctrl(req, res, next);
+      } catch (error) {
+        next(error);
+      }
+    };
+  };
+  
+  module.exports = ctrlWrapper;
+  
