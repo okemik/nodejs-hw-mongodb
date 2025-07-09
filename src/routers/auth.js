52ccacf190f67be5f2b64206f3b2dfa4155e57f5
okemik <okemik@yahoo.com>
2025-07-10 00:54:33 +0200

    routers/auth.js updated

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/auth');
 
+router.post('/register', authController.register);
 router.post('/send-reset-email', controller.sendResetEmail);
 router.post('/reset-pwd', controller.resetPassword);
+
 
 module.exports = router;
